d5d519d5e729f9cd40870b311fca27e519246aa4
MAJIDHATAMI <hatamiii.info@gmail.com>
2024-03-11 19:21:31 +0330

    forecast weather current/coordinate created $ styled & extended

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 const API_KEY = "3c747250846fc97b5e99f5bc3fab7c33";
+const DAYS = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 const searchInput = document.querySelector("input");
 const searchButton = document.querySelector("button");
 const weatherContainer = document.getElementById("weather");
+const forecastContainer = document.getElementById("forecast");
 const locationIcon = document.getElementById("location");
 
 const getCurrentWeatherByname = async (city) => {
@@ -15,6 +25,20 @@ const getCurrentWeatherByname = async (city) => {
 
 const getCurrentWeatherByCoordinates = async (lat, lon) => {
   const url = `${BASE_URL}/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const response = await fetch(url);
+  const json = await response.json();
+  return json;
+};
+
+const getForecastWeatherByCoordinates = async (lat, lon) => {
+  const url = `${BASE_URL}/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const response = await fetch(url);
+  const json = await response.json();
+  return json;
+};
+
+const getForecastWeatherByname = async (city) => {
+  const url = `${BASE_URL}/forecast?q=${city}&appid=${API_KEY}&units=metric`;
   const response = await fetch(url);
   const json = await response.json();
   return json;
@@ -42,19 +66,43 @@ const renderCurrentWeather = (data) => {
   weatherContainer.innerHTML = weatherJSX;
 };
 
+const renderForecastWeather = (data) => {
+  forecastContainer.inn`erHTML = "";
+  data = data.list.filter((obj) => obj.dt_txt.endsWith("12:00:00"));
+  data.forEach((i) => {
+    const forecastJSX = `
+      <div>
+        <img alt = "${i.weather[0].main}" 
+        src ="https://api.openweathermap.org/img/w/${i.weather[0].icon}.png"/>
+        <h3>${DAYS[new Date(i.dt * 1000).getDay()]}</h3>
+        <p>${Math.round(i.main.temp)} °C</p>
+        <span>${i.weather[0].main}</span>
+      </div>
+    `;
+    forecastContainer.innerHTML += forecastJSX;
+  });
+};
+
 const searchHandler = async () => {
   const cityName = searchInput.value;
   if (!cityName) {
     alert("pls add city name");
   }
   const currentData = await getCurrentWeatherByname(cityName);
+  const forecastData = await getForecastWeatherByname(cityName);
   renderCurrentWeather(currentData);
+  renderForecastWeather(forecastData);
 };
 
 const positionCallback = async (position) => {
   const { latitude, longitude } = position.coords;
   const currentData = await getCurrentWeatherByCoordinates(latitude, longitude);
   renderCurrentWeather(currentData);
+  const forecastData = await getForecastWeatherByCoordinates(
+    latitude,
+    longitude
+  );
+  renderForecastWeather(forecastData);
 };
 const errorCallback = (error) => {
   console.log(error.message);
